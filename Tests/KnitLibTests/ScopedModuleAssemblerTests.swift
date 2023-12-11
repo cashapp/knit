9f8d072b524df4973f4b66e292eed028eb8a01b2
Alex Skorulis <askorulis@squareup.com>
2023-12-11 14:28:12 +1100

    Fix assemblyValidation not taking into account having a parent assembler

--- Tests/KnitLibTests/ScopedModuleAssemblerTests.swift
+++ Tests/KnitLibTests/ScopedModuleAssemblerTests.swift
@@ -14,6 +14,15 @@ final class ScopedModuleAssemblerTests: XCTestCase {
         XCTAssertEqual(assembler.internalAssembler.registeredModules.count, 1)
     }
 
+    func testParentExcluded() throws {
+        let parent = try ScopedModuleAssembler<TestResolver>(_modules: [Assembly1()])
+        let assembler = try ScopedModuleAssembler<OutsideResolver>(
+            parent: parent.internalAssembler,
+            _modules: [Assembly3()]
+        )
+        XCTAssertEqual(assembler.internalAssembler.registeredModules.count, 1)
+    }
+
     func testPostAssemble() throws {
         let assembler = try ScopedModuleAssembler<TestResolver>(_modules: [Assembly1()]) { container in
             container.register(String.self) { _ in "string" }
@@ -52,5 +61,11 @@ protocol OutsideResolver: Resolver { }
 private struct Assembly2: AutoInitModuleAssembly {
     typealias TargetResolver = OutsideResolver
     static var dependencies: [any ModuleAssembly.Type] { [] }
+    func assemble(container: Container) { }
+}
+
+private struct Assembly3: AutoInitModuleAssembly {
+    typealias TargetResolver = OutsideResolver
+    static var dependencies: [any ModuleAssembly.Type] { [Assembly1.self] }
     func assemble(container: Container) { }
 }
