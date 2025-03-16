9973007bac0d033ef5a333ad80c7331731474179
Alex Skorulis <askorulis@squareup.com>
2025-03-17 08:58:44 +1100

    Convert Container registration to use parameter packs

--- Sources/Swinject/Container.swift
+++ Sources/Swinject/Container.swift
@@ -99,28 +99,6 @@ public final class Container {
         resetObjectScope(objectScope as ObjectScopeProtocol)
     }
 
-    /// Adds a registration for the specified service with the factory closure to specify how the service is
-    /// resolved with dependencies.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to register.
-    ///   - name:        A registration name, which is used to differentiate from other registrations
-    ///                  that have the same service and factory types.
-    ///   - factory:     The closure to specify how the service type is resolved with the dependencies of the type.
-    ///                  It is invoked when the ``Container`` needs to instantiate the instance.
-    ///                  It takes a ``Resolver`` to inject dependencies to the instance,
-    ///                  and returns the instance of the component type for the service.
-    ///
-    /// - Returns: A registered ``ServiceEntry`` to configure more settings with method chaining.
-    @discardableResult
-    public func register<Service>(
-        _ serviceType: Service.Type,
-        name: String? = nil,
-        factory: @escaping (Resolver) -> Service
-    ) -> ServiceEntry<Service> {
-        return _register(serviceType, factory: factory, name: name)
-    }
-
     /// This method is designed for the use to extend Swinject functionality.
     /// Do NOT use this method unless you intend to write an extension or plugin to Swinject framework.
     ///
@@ -426,4 +404,31 @@ extension Container: CustomStringConvertible {
 
 private extension Container {
     static let graphIdentifierKey = ServiceKey(serviceType: GraphIdentifier.self, argumentsType: Resolver.self)
+}
+
+// MARK: Registration
+
+extension Container {
+
+    @discardableResult
+    public func register<Service, each ArgumentType>(
+        _ serviceType: Service.Type,
+        name: String? = nil,
+        factory: @escaping (Resolver, repeat each ArgumentType) -> Service
+    ) -> ServiceEntry<Service> {
+        return _register(serviceType, factory: factory, name: name)
+    }
+
+    @discardableResult
+    public func register<Service, each ArgumentType>(
+        _ serviceType: Service.Type,
+        name: String? = nil,
+        mainActorFactory: @escaping @MainActor (Resolver, repeat each ArgumentType) -> Service
+    ) -> ServiceEntry<Service> {
+        return register(serviceType) { (resolver: Resolver, arguments: repeat each ArgumentType) in
+            MainActor.assumeIsolated {
+                return mainActorFactory(resolver, repeat each arguments)
+            }
+        }
+    }
 }
